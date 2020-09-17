7bebdb7de87c42644a74623a4d3aa65efd11f6bf
Diogo-V <diogo.venancio00@gmail.com>
2020-09-17 15:54:19 +0100

    Updated deletion.py so that it would reflect the changes made to the database
    Took 29 minutes

--- database/src/query/databaseNotebookTabs/pets.py
+++ database/src/query/databaseNotebookTabs/pets.py
@@ -110,27 +110,21 @@ def getsRequestedPets(queryInfo):
         """Checks the type of query that we need to make and gets it."""
 
         # Extracts components
-        [petName, petType, clientName] = queryInfo
+        [petName, petType, petBreed, clientName] = queryInfo
 
-        if petName == '':
-            if petType == '':
-                return f"clients.name like '%{clientName}%'"
-            elif clientName == '':
-                return f"animals.type like '%{petType}%'"
-            else:
-                return f"animals.type like '%{petType}%' and clients.name like '%{clientName}%'"
-        else:
-            if petType == '':
-                if clientName == '':
-                    return f"animals.name like '%{petName}%'"
-                else:
-                    return f"animals.name like '%{petName}%' and clients.name like '%{clientName}%'"
-            else:
-                if clientName == '':
-                    return f"animals.name like '%{petName}%' and animals.type like '%{petType}%'"
-                else:
-                    return f"animals.name like '%{petName}%' and animals.type like '%{petType}%' " \
-                           f"and clients.name like '%{clientName}%'"
+        myQuery = ''
+
+        # Gets query
+        if petName != '':
+            myQuery += f"animals.name like '%{petName}%' and "
+        if petType != '':
+            myQuery += f"animals.type like '%{petType}%' and "
+        if petBreed != '':
+            myQuery += f"animals.breed like '%{petBreed}%' and "
+        if clientName != '':
+            myQuery += f"clients.name like '%{clientName}%' and "
+
+        return myQuery[:-5]
 
     # Creates a connection to our database and a cursor to work with it
     connection = connect("database/database.sqlite")
--- interface/databaseNotebookTabs/popupWindows/pets/deletion.py
+++ interface/databaseNotebookTabs/popupWindows/pets/deletion.py
@@ -24,17 +24,17 @@ class WindowDeletePet(Toplevel):
         # Creates toplevel window that will be displayed. Sets size and blocks resize
         Toplevel.__init__(self, master)
         self.title('Apagar animal')
-        self.geometry("1000x500")
+        self.geometry("1250x600")
         self.resizable(False, False)
         self.transient(master)
 
         # Creates tab main window and puts it on the screen
-        self.window = Frame(self, height=500, width=1000)
+        self.window = Frame(self, height=600, width=1250)
         self.window.pack(fill='both', expand=True)
 
         # Creates a search frame and a display frame and puts them on the screen
-        self.search = LabelFrame(self.window, text=' Pesquisar ', width=1000, height=100)
-        self.display = LabelFrame(self.window, text=' Animais ', width=1000, height=300)
+        self.search = LabelFrame(self.window, text=' Pesquisar ', width=1250, height=100)
+        self.display = LabelFrame(self.window, text=' Animais ', width=1250, height=500)
         self.search.pack(padx=10, pady=10, fill="both", expand=True)
         self.display.pack(padx=10, pady=(0, 10), fill="both", expand=True)
 
@@ -50,41 +50,47 @@ class WindowDeletePet(Toplevel):
         # Allocates memory for the entry values
         petName = StringVar()
         petType = StringVar()
+        petBreed = StringVar()
         clientName = StringVar()
 
         # Creates labels and entry fields and puts them on the screen
         self.labelPetName = Label(self.search, text='Nome:')
-        self.labelPetName.pack(side=LEFT, padx=(25, 5), pady=10)
+        self.labelPetName.pack(side=LEFT, padx=(25, 5), pady=5)
         self.entryPetName = Entry(self.search, textvariable=petName)
-        self.entryPetName.pack(side=LEFT, padx=(0, 5), pady=10)
+        self.entryPetName.pack(side=LEFT, padx=(0, 5), pady=5)
         self.labelPetType = Label(self.search, text='Tipo:')
-        self.labelPetType.pack(side=LEFT, padx=(10, 5), pady=10)
-        self.comboboxPetType = Combobox(self.search, textvariable=petType, state="readonly", values=[''] + typeOfAnimal)
-        self.comboboxPetType.pack(side=LEFT, padx=(0, 5), pady=10)
+        self.labelPetType.pack(side=LEFT, padx=(10, 5), pady=5)
+        self.boxPetType = Combobox(self.search, textvariable=petType, state="readonly", values=[''] + typeOfAnimal)
+        self.boxPetType.pack(side=LEFT, padx=(0, 5), pady=5)
+        self.labelPetBreed = Label(self.search, text='Raça:')
+        self.labelPetBreed.pack(side=LEFT, padx=(10, 5), pady=5)
+        self.entryPetBreed = Entry(self.search, textvariable=petBreed)
+        self.entryPetBreed.pack(side=LEFT, padx=(0, 5), pady=5)
         self.labelClientName = Label(self.search, text='Cliente:')
-        self.labelClientName.pack(side=LEFT, padx=(10, 5), pady=10)
+        self.labelClientName.pack(side=LEFT, padx=(10, 5), pady=5)
         self.entryClientName = Entry(self.search, textvariable=clientName)
-        self.entryClientName.pack(side=LEFT, padx=(0, 15), pady=10)
+        self.entryClientName.pack(side=LEFT, padx=(0, 15), pady=5)
 
         # Creates search button and puts it on the screen
         self.button = Button(self.search, text='Procurar', command=self.updateTree)
-        self.button.pack(side=LEFT, padx=(130, 50), pady=10)
+        self.button.pack(side=LEFT, padx=(130, 50), pady=5)
 
         # Columns names that are going to be inserted inside the tree
-        columns = ('', 'Nome do animal', 'Nome do cliente', 'Tipo de animal', 'Peso', 'Tipo de pelo')
+        columns = ('', 'Nome do animal', 'Nome do cliente', 'Tipo de animal', 'Raça', 'Peso', 'Tipo de pelo')
 
         # Creates tree that will display all the links
-        self.tree = Treeview(self.display, columns=columns, height=13, show='headings')
-        self.tree.pack(side=LEFT, padx=10, pady=10)
+        self.tree = Treeview(self.display, columns=columns, height=18, show='headings')
+        self.tree.pack(side=LEFT, padx=10, pady=5)
 
         # Formats columns
         self.tree.column("#0", stretch=NO, anchor='center', width=0)
         self.tree.column(0, stretch=NO, anchor='center', width=0)
-        self.tree.column(1, stretch=NO, anchor='center', width=184)
-        self.tree.column(2, stretch=NO, anchor='center', width=184)
-        self.tree.column(3, stretch=NO, anchor='center', width=184)
-        self.tree.column(4, stretch=NO, anchor='center', width=184)
-        self.tree.column(5, stretch=NO, anchor='center', width=184)
+        self.tree.column(1, stretch=NO, anchor='center', width=197)
+        self.tree.column(2, stretch=NO, anchor='center', width=197)
+        self.tree.column(3, stretch=NO, anchor='center', width=197)
+        self.tree.column(4, stretch=NO, anchor='center', width=197)
+        self.tree.column(5, stretch=NO, anchor='center', width=197)
+        self.tree.column(6, stretch=NO, anchor='center', width=197)
 
         # Define columns heading and sets their sorting function
         for col in columns:
@@ -169,7 +175,7 @@ class WindowDeletePet(Toplevel):
         Description:
         > Gets values inside each entry box and creates a list with those values.
         """
-        return [self.entryPetName.get(), self.comboboxPetType.get(), self.entryClientName.get()]
+        return [self.entryPetName.get(), self.boxPetType.get(), self.entryPetBreed.get(), self.entryClientName.get()]
 
     def updateTree(self):
         """
@@ -178,15 +184,15 @@ class WindowDeletePet(Toplevel):
         """
 
         # Gets information in entries
-        [petName, petType, clientName] = self.getsEntries()
+        [petName, petType, petBreed, clientName] = self.getsEntries()
 
         # If no information was typed, just refresh page
-        if petName == '' and petType == '' and clientName == '':
+        if petName == '' and petType == '' and clientName == '' and petBreed == '':
             self.refreshTree()
         else:
 
             # Gets rows that are going to be displayed
-            rows = getsRequestedPets([petName, petType, clientName])
+            rows = getsRequestedPets([petName, petType, petBreed, clientName])
 
             # Displays our queried rows
             self.displayTreeRows(rows)
