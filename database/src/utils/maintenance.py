9847390971a7c0c362859181e21d718eb15a1528
Diogo-V <diogo.venancio00@gmail.com>
2020-09-25 21:02:01 +0100

    Fixed a bug where we were not cleaning the database properly
    Took 1 hour 18 minutes

--- database/src/utils/maintenance.py
+++ database/src/utils/maintenance.py
@@ -45,9 +45,7 @@ def movesAppointmentsToHistory():
     except Error:
 
         # Error information and details processing
-        print(type(Error))
-        print(Error.args)
-        print(Error)
+        print("ERROR: movesAppointmentsToHistory")
 
         mainDBConn.rollback()  # Removes any change made during execution
         historyDBConn.rollback()  # Removes any change made during execution
@@ -70,19 +68,29 @@ def clearsElementsWithNoLinks():
 
     try:
 
-        # SQL syntax that is going to be parsed inside the database console
+        # SQL syntax that deletes clients with no pets
         queryDeleteClients = f"""
                               delete from clients where clients.ROWID not in 
                               (select clients.ROWID from clients inner join petsClientsLink 
                               where petsClientsLink.clientId = clients.ROWID)
                               """
 
-        # SQL syntax that is going to be parsed inside the database console
+        # SQL syntax that deletes pets with no owners
         queryDeletePets = f"""
                            delete from animals where animals.ROWID not in 
                            (select animals.ROWID from animals inner join petsClientsLink 
                            where petsClientsLink.petId = animals.ROWID)
                            """
+
+        # SQL syntax that deletes links which hold info that doesn't exist
+        queryDeleteLinks = f"""
+                            delete from petsClientsLink where petsClientsLink.ROWID not in 
+                            (select petsClientsLink.ROWID from petsClientsLink inner join clients, animals where
+                            petsClientsLink.petId = animals.ROWID and petsClientsLink.clientId = clients.ROWID)
+                            """
+
+        # Executes links deletion command
+        cursor.execute(queryDeleteLinks)
 
         # Executes clients deletion command
         cursor.execute(queryDeleteClients)
@@ -96,9 +104,7 @@ def clearsElementsWithNoLinks():
     except Error:
 
         # Error information and details processing
-        print(type(Error))
-        print(Error.args)
-        print(Error)
+        print("ERROR: clearsElementsWithNoLinks")
 
         connection.rollback()  # Removes any change made during execution
 
